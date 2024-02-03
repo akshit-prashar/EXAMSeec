0bede07ae9cd46153cacfd2319a1061f1c90e430
akshit-prashar <158741796+akshit-prashar@users.noreply.github.com>
2024-02-03 18:19:49 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,38 @@ var messageBox = document.querySelector('.js-message');
           card.classList.remove('active');
       });
   });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('.profile-card-social__item');
+
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const url = this.getAttribute('href');
+      openPopup(url);
+    });
+  });
+
+  // Close popup when clicking the close button
+  document.querySelector('.close').addEventListener('click', function() {
+    closePopup();
+  });
+});
+
+function openPopup(url) {
+  const popupContainer = document.getElementById('popupContainer');
+  const popupIframe = document.getElementById('popupIframe');
+  popupIframe.src = url;
+  popupContainer.style.display = 'block';
+}
+
+function closePopup() {
+  const popupContainer = document.getElementById('popupContainer');
+  const popupIframe = document.getElementById('popupIframe');
+  popupIframe.src = '';
+  popupContainer.style.display = 'none';
+}
